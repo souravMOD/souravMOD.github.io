945f1bfc2d657ab3154f9c7747443bfb77fd9355
sourav <souravv.modakk@gmail.com>
2026-02-02 23:18:00 +0100

    Fix basepath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/personal_web',
   images: {
     domains: ['images.unsplash.com'],
     unoptimized: true,
